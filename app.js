3a76351f537f031fa507c6e3851ea01cd44a0100
LeviKull <Levikull@hotmail.com>
2019-07-05 14:28:42 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// val = Math.floor(Math.random() * 20 + 1);
+
+const firstName = "Levi ";
+const lastName = "Kull"; 
+const age = 33;
+
+let val;
+let test;
+
+val = firstName + ' ' + lastName;
+val = 'Tom ';
+val += 'Delaney';
+
+val = 'Hello, my name is ' + firstName + lastName + ' and I am ' + age;
+
+test = val.toLocaleUpperCase();
+
+console.log(test);
